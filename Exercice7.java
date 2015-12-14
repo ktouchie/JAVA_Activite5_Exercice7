ef7f5793aa05a59894fa592ce4391bb0d8c2f3d7
Karin Touchie <ktouchie@gmail.com>
2015-12-14 15:44:48 +0100

    COmpile

--- Exercice7.java
+++ Exercice7.java
@@ -1,17 +1,26 @@
 import java.util.*;
-class Exercice {
+class Exercice7 {
     
     public static void main(String [] args) {
         Scanner sc = new Scanner(System.in);
         
         System.out.println("Veuillez saisir un nombre naturel:"); //* Get number */
-        int n = sc.nextInt(); //* Store number as n */
-        String str = Integer.toString(n); //* Store n as string *//
-        int a[] = new int[str.length] //* Declare array with as many elements as n has digits */
+        int num = sc.nextInt(); //* Store number as n */
+        String str = Integer.toString(num); //* Store n as string *//
         
-        while (num>0) {
-            
+        char digit[] = {'0','1','2','3','4','5','6','7','8','9'};
+        int count = 0;
+        
+        for (int i=0; i<10; i++) {
+            for (int j=0; j<(str.length()); j++) {
+                if (str.charAt(j) == digit[i]) {
+                    count++;
+                }
+            }
+            if (count>0) {
+                System.out.println(digit[i] + " apparait " + count + " fois.");
+                count = 0;
+            }            
         }
-        System.out.println("La somme des chiffres de " + n + " est " + sum);
     }
 }
