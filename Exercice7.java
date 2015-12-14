108f67ef4a1ef6622c139d17c0ef54062cc3ebdf
Karin Touchie <ktouchie@gmail.com>
2015-12-14 14:47:13 +0100

    Create file

---
+++ Exercice7.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Exercice {
+    
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Veuillez saisir un nombre naturel:"); //* Get number */
+        int n = sc.nextInt(); //* Store number as n */
+        String str = Integer.toString(n); //* Store n as string *//
+        int a[] = new int[str.length] //* Declare array with as many elements as n has digits */
+        
+        while (num>0) {
+            
+        }
+        System.out.println("La somme des chiffres de " + n + " est " + sum);
+    }
+}
